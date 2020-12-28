99e3b75689f3f31e0dfa1758688ed53ce1379443
ZULQARNAIN.SALEEM <ZULQARNAIN.SALEEM@systemsltd.com>
2020-12-29 00:21:38 +0500

    Add new Application WebAPI

---
+++ source/MNBEC.API.WEBAPI/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using MNBEC.API.Core.Extensions;
+
+namespace MNBEC.API.WEBAPI
+{
+    /// <summary>
+    /// Program class is starting point of application.
+    /// </summary>
+    public class Program
+    {
+        #region Methods
+        /// <summary>
+        /// Main is starting point for application.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        /// <summary>
+        /// CreateWebHostBuilder configure and initialize Web Host.
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging()
+                .UseStartup<Startup>();
+
+        #endregion
+    }
+}
