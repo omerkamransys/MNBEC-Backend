4d5bba9e741e1384e255d3886641ce96494fccef
ZULQARNAIN.SALEEM <ZULQARNAIN.SALEEM@systemsltd.com>
2021-02-01 16:31:45 +0500

    update GetListByStakeholderId logic

--- source/LevelGetListByStakeholderId.sql
+++ source/LevelGetListByStakeholderId.sql
@@ -15,15 +15,19 @@ As
 Begin
 Set nocount on;
 
-	Select l.LevelId,l.LevelName,l.ParentId,qt.Id as QuestionaireTemplateId, 
-	qt.Title as QuestionaireTemplateName , shqs.IsSubmit  from StakeholderLevel sl with (nolock)
-	inner join level l with (nolock) on sl.LevelId = l.LevelId and sl.Active = 1 and l.Active = 1
+	Select Distinct l.LevelId,l.LevelName,l.ParentId,qt.Id as QuestionaireTemplateId, 
+	qt.Title as QuestionaireTemplateName , shqs.IsSubmit  
+	from level l with (nolock)
 	inner join QuestionnaireTemplate qt with (nolock) on l.QuestionaireTemplateId = qt.Id
+	and l.Active = 1 and qt.Active = 1
+	inner join Question q with (nolock) on qt.Id = q.QuestionaireTemplateId and q.Active = 1
+	left join StakeholderLevel sl with (nolock) on sl.LevelId = l.LevelId and sl.Active = 1
 	left join StakeholderQuestionnaireStatus shqs with (nolock) on  shqs.QuestionaireTemplateId = qt.Id
 	and shqs.LevelId = l.LevelId and shqs.StakeholderId = @PStakeholderId
-	where sl.UserId = @PStakeholderId and sl.Active = 1 and l.Active = 1;
+	where (sl.UserId = @PStakeholderId and sl.Active = 1 and q.Responsible is null) or 
+	(q.Responsible in (Select RoleId from applicationuserrole where UserId = @PStakeholderId
+		and Active = 1))  ;
 
-	
 End;
 
 GO
