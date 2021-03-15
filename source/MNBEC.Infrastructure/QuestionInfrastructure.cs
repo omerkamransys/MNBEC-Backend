90c3eddcfddcf31f727378d652bc45bc1b8aa4f3
Mehran Khan <mehran.khan@systemsltd.com>
2021-03-16 03:06:27 +0500

    question element changed to string

--- source/MNBEC.Infrastructure/QuestionInfrastructure.cs
+++ source/MNBEC.Infrastructure/QuestionInfrastructure.cs
@@ -535,7 +535,7 @@ namespace MNBEC.Infrastructure
             List<string> BulkQuestions = new List<string>();
             foreach (var item in questions)
             {
-                BulkQuestions.Add(string.Format("({0},{1},{2},{3},{4},'{5}','{6}','{7}','{8}','{9}',{10},GETUTCDATE(),'1',{11},{12})",
+                BulkQuestions.Add(string.Format("({0},{1},{2},{3},{4},'{5}','{6}','{7}','{8}','{9}',{10},GETUTCDATE(),'1','{11}',{12})",
                     item.QuestionaireTemplateId,
                     GetCreatedById(item.Area),
                     GetCreatedById(item.FourP),
